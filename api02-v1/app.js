a098668f04e4b72e4d833254b96133bf694f777d
“Cecília <ceci.miranda254@gmail.com>
2023-06-29 08:27:53 -0300

    foto

--- api02-v1/app.js
+++ api02-v1/app.js
@@ -5,8 +5,10 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
-const senhaToken = "Cecili@2003"
-const {decode} = require('punycode')
+const senhaToken = "Cecili@2003";
+const {decode} = require('punycode');
+const formidable = require('formidable');
+const fs = require('fs');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,6 +28,21 @@ connection.connect((err) => {
     else {
         console.log('Conectado ao banco de dados com sucesso!');
     }
+});
+
+app.post('/usuarios', (req, res) => {
+    const NomeUsuario = req.body.nomeUsuario;
+    const LoginNome = req.body.LoginNome;
+    const senha = encriptarSenha(req.body.senha);
+    connection.query('INSERT INTO usuarios (NomeUsuario, LoginNome, senha) VALUES (?,?,?)',[NomeUsuario, LoginNome, senha], (error, rows) => {
+        if(error) {
+            console.log('Erro ao processar o comando SQL.', error.message);
+        }
+        else
+             {
+                res.status(403).json({messageErro: "Cadastrado com sucesso!"})
+            }
+    });
 });
 
 app.get('/usuarios', verficarToken, (req, res) => {
@@ -82,6 +99,55 @@ function encriptarSenha(senha){
     return senhaencriptada
 }
 
+app.put('/foto/:id', (req, res) => {
+    const id = req.params['id'];
+    const formulario = new formidable.IncomingForm();
+    formulario.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+      } else {
+        const caminhoOriginal = files.arquivo[0].filepath;
+        console.log(caminhoOriginal);
+        const imagem = fs.readFileSync(caminhoOriginal);
+        const sql = 'UPDATE usuarios SET foto = ? WHERE codusuario = ?';
+        connection.query(sql, [imagem, id], (err, result) => {
+          if (err) {
+            res
+              .status(400)
+              .json({
+                mensagem: `Erro ao gravar mensagem. Erro: ${err.message}`,
+              });
+            throw err;
+          } else {
+            console.log('Imagem gravada com sucesso!');
+            res.status(200).json({ mensagem: 'Imagem gravada com sucesso.' });
+          }
+        });
+      }
+    });
+  });
+
+  app.get('/foto/:id', (req, res) => {
+    const id = req.params['id'];
+    const sql = 'SELECT foto FROM usuarios WHERE codusuario = ?';
+    connection.query(sql, [id], (err, result) => {
+      if (err) {
+        throw err;
+      }
+  
+      if (result.length > 0) {
+        const foto = result[0].foto;
+        const fs = require('fs');
+        fs.writeFileSync('foto.jpg', foto);
+        //console.log('Imagem recuperada e salva com sucesso!');
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(foto, 'binary');
+      } else {
+        console.log('Nenhuma imagem encontrada com o ID fornecido.');
+      }
+    });
+  });
+  
 
 app.post('/login', (req, res) => {
     const LoginNome = req.body.LoginNome;
@@ -102,21 +168,6 @@ app.post('/login', (req, res) => {
     });
 });
 
-
-app.post('/usuarios', (req, res) => {
-    const NomeUsuario = req.body.nomeUsuario;
-    const LoginNome = req.body.LoginNome;
-    const senha = encriptarSenha(req.body.senha);
-    connection.query('INSERT INTO usuarios (NomeUsuario, LoginNome, senha) VALUES (?,?,?)',[NomeUsuario, LoginNome, senha], (error, rows) => {
-        if(error) {
-            console.log('Erro ao processar o comando SQL.', error.message);
-        }
-        else
-             {
-                res.status(403).json({messageErro: "Cadastrado com sucesso!"})
-            }
-    });
-});
 
 app.listen(3000, () => {
     console.log('Servidor de API funcionando na porta 3000');
